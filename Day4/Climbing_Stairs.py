6223fcc7e44d98238a100f01dc317b0d35f0472a
Devansh Chaubey <86419106+Devansh-46@users.noreply.github.com>
2024-07-18 22:34:49 +0530

    Delete Day4 directory

--- Day4/Climbing_Stairs.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        step_one = 1   
-        step_two = 1
-        for i in range(n-1):
-            temp = step_one
-            step_one += step_two
-            step_two = temp
-        return step_one

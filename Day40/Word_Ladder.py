151c04def456bfe230257c8032e5ba3ffbd5aa54
Devansh Chaubey <86419106+Devansh-46@users.noreply.github.com>
2024-08-23 23:22:08 +0530

    Create Word_Ladder.py

---
+++ Day40/Word_Ladder.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+class Solution:
+    def ladderLength(self, begin_word: str, end_word: str, word_list: List[str]) -> int:
+        if begin_word not in word_list:
+            word_list.append(begin_word)
+        patterns = defaultdict(set)
+        for word in word_list:
+            for i in range(len(word)):
+                form = word[:i] + "*" + word[i+1:]
+                patterns[form].add(word)
+    
+        # Create a graph between words
+        graph = defaultdict(list)
+        for word in word_list:
+            local_set = set()
+            for i in range(len(word)):
+                form = word[:i] + "*" + word[i+1:]
+                local_set |= patterns[form]
+            graph[word] = list(local_set)
+        # find the shortest way between begin_word and end_word
+        # -> BFS
+        queue = deque([begin_word])
+        step = 0
+        visit = set([begin_word])
+        while queue:
+            step += 1
+            for _ in range(len(queue)):
+                choosen_word = queue.popleft()
+                if choosen_word == end_word:
+                    return step
+                for next_word in graph[choosen_word]:
+                    if next_word in visit:
+                        continue
+                    visit.add(next_word)
+                    queue.append(next_word)
+        return 0
